eceeb84854e49897c2fa17e99b44197e86091858
johnwithane <johnnyajansen@gmail.com>
2025-10-13 23:06:15 -0700

    new grid images

--- js/hero-grid.js
+++ js/hero-grid.js
@@ -27,7 +27,7 @@
     // Desktop code only below this line
     const GRID_FOLDER = 'videos/grid/';
     const GRID_PREFIX = 'grid ';
-    const NUM_IMAGES = 32;
+    const NUM_IMAGES = 57;
     const CYCLE_INTERVAL = 4000;
     
     function calculateGridSize() {
